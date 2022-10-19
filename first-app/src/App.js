db23ff324a4a4a8eb2284d7b85178d9e017463fc
Vivek <dhimanvivek10@gmail.com>
2022-10-19 23:31:03 +0530

    few more corrections

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     color: "Black",
     backgroundColor: 'White'
 })
- const [btnText, setBtnText] = useState('DarkMode')
+ const [btnText, setBtnText] = useState('DarkMode Off')
  const [navMode, setNavMode] = useState("navbar navbar-expand-lg bg-primary")
 
   let toggle = ()=>{
@@ -16,27 +16,30 @@ export default function App() {
           background: '#121211'
       });
       document.body.style.backgroundColor = '#121211';
-      setBtnText('LightMode')
+      document.getElementById('exampleFormControlTextarea1').style.backgroundColor = '#252625';
+      document.getElementById('exampleFormControlTextarea1').style.color = 'white';
+      setBtnText('DarkMode On')
       setNavMode("navbar navbar-expand-lg bg-dark")
-      
     }
     else {
       setStyle({
         color: "Black",
         background: 'White'
     });
-    setBtnText('DarkMode')
+    setBtnText('DarkMode Off')
     setNavMode("navbar navbar-expand-lg bg-primary")
     document.body.style.backgroundColor = 'white';
+    document.getElementById('exampleFormControlTextarea1').style.backgroundColor = 'white';
+    document.getElementById('exampleFormControlTextarea1').style.color = 'black';
   }
 }
   return (
     <>
     <div style={myStyle}>
-      <nav className={navMode} style={myStyle} color="white">
+      <nav className={navMode} color="white">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            Text Editor
+          <i className="bi bi-card-text"></i> Text Editor
           </a>
           <button
             className="navbar-toggler"
@@ -50,17 +53,9 @@ export default function App() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  About
-                </a>
-              </li>
+              
             </ul>
             <form className="d-flex" role="search">
               <div className="form-check form-switch mx-2" onClick = {toggle}>
--- first-app/src/components/TextArea.js
+++ first-app/src/components/TextArea.js
@@ -2,51 +2,84 @@ import React, { useState } from "react";
 import App from "../App.js";
 
 export default function TextArea() {
-  const msg = new SpeechSynthesisUtterance()
-  const [text, setText] = useState("");
+  const msg = new SpeechSynthesisUtterance();
+  const [text, setText] = useState(""); //Usestate used to manipulate text(textarea) in numrous ways
   const onChange = (event) => {
-    setText(event.target.value)
+    setText(event.target.value);
   };
-  const upclick = () => {
+  const upclick = () => { //function made to send uppercase string in seText use state
     let ntext = text.toUpperCase();
     setText(ntext);
   };
-  const dwclick = () => {
+  const dwclick = () => { //function made to send lowercase string in seText use state
     let ntext = text.toLowerCase();
     setText(ntext);
   };
-  const rmvspcs = ()=>{
-    let ntext = text.replace(/\s+/g, ' ').trim()
-    setText(ntext)
-  }
-  const clr = ()=>{
-    setText('')
-  }
-  const speechHandler = (msg) => {
-    msg.text = text
-    window.speechSynthesis.speak(msg)
-  }
+  const rmvspcs = () => { //function made to remove spaces in string and send to seText use state
+    let ntext = text.replace(/\s+/g, " ").trim();
+    setText(ntext);
+  };
+  const clr = () => { //function made to replace whole string in seText use state with blank
+    setText("");
+  };
+
+   const italic = () => { //function made to replace selected words to italic
+      document.getElementById("exampleFormControlTextarea1").addEventListener('mouseup',(evt)=>{
+         let t = evt.currentTarget;
+         let selt = t.value.substr(t.selectionStart, t.selectionEnd - t.selectionStart); 
+         console.log(selt) //to-do fix this button and then put bold underline logic as well
+     });
+   };
+  const speechHandler = (msg) => { //function made to speak the string in textarea
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  };
   return (
-    <div className='container-fluid'style={App.myStyle}>
+    <div className="container-fluid" style={App.myStyle}>
       <div className="mb-3">
         <textarea
           className="form-control"
           id="exampleFormControlTextarea1"
           rows="15"
-          value = {text}
+          value={text}
           placeholder="Enter the text here"
           onChange={onChange}
           style={App.myStyle}
         ></textarea>
       </div>
       <div className="container mx-2 my-2">
-        <button className="btn btn-outline-primary mx-1" onClick={upclick}>Uppercase</button>
-        <button className="btn btn-outline-primary mx-1"onClick={dwclick}>Lowercase</button>
-        <button className="btn btn-outline-primary mx-1"onClick={rmvspcs}>Remove spaces</button>
-        <button className="btn btn-outline-primary mx-1"onClick={() => speechHandler(msg)}>Text to Speech</button>
-        <button className="btn btn-outline-primary mx-1"onClick={clr}>clear</button>
+        <button className="btn btn-outline-primary mx-1" onClick={upclick}>
+          Uppercase
+        </button>
+        <button className="btn btn-outline-primary mx-1" onClick={dwclick}>
+          Lowercase
+        </button>
+        <button className="btn btn-outline-primary mx-1" onClick={rmvspcs}>
+          Remove spaces
+        </button>
+        <button
+          className="btn btn-outline-primary mx-1"
+          onClick={() => speechHandler(msg)}
+        >
+          Speak Text
+        </button>
+        <button className="btn btn-outline-primary mx-1" onClick={italic}>
+          <i className="bi bi-type-italic"></i>
+        </button>
+        <button className="btn btn-outline-primary mx-1" onClick={clr}>
+          <i className="bi bi-type-bold"></i>
+        </button>
+        <button className="btn btn-outline-primary mx-1" onClick={clr}>
+          <i className="bi bi-type-underline"></i>
+        </button>
+        <button className="btn btn-outline-primary mx-1" onClick={clr}>
+          clear
+        </button>
       </div>
-      <em className="text">{text.length} characters {text.split(' ').length-1} words You can read it in {text.split(' ').length * 0.008} mins</em>
+      <em className="text">
+        {text.length} characters, {text.split(" ").length - 1} words, You can read
+        it in {(text.split(" ").length - 1) * 0.008} mins
+      </em>
     </div>
   );
 }
